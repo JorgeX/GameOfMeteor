01689528c499f260757f9297306aecbdfa97f0cf
Jörge Ojanen <jorge.ojanen@sysart.fi>
2014-12-27 23:10:46 +0200

    3.2 show ongoing games. Buuut wont...

--- client/home.js
+++ client/home.js
@@ -5,7 +5,7 @@ Template.userList.helpers({
 
 		//haetaan avoimien pelin vastustajien id:t
 		Games.find({inProgress: true}).forEach(function(game){
-			excluded.push(game.currentTurn[game.currentTurn[0] === myId ? 1 : 0]);
+			excluded.push(otherId(game));
 		});
 		return Meteor.users.find({_id: {$not: { $in: excluded}}});
 	}
@@ -16,3 +16,19 @@ Template.userItem.events({
 		Meteor.call('createGame', template.data._id);
 	}
 });
+
+Template.gameList.helpers({
+	games: function(){
+		return Games.find({inProgress: true}).map(function(game){
+			game.otherPlayer = Meteor.users.findOne(otherId(game)).username;
+			game.started = moment(game.started).fromNow();
+			console.log('Game: ' + game);
+			return game;
+		});
+	}
+});
+
+function otherIf(game){
+ return game.currentTurn[game.currentTurn[0] === Meteor.userId() ? 1 : 0];
+}
+
